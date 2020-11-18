47759eca00e282ad77cf619dda31d0824091b1ea
vik-ash <vikashvermacom92@gmail.com>
2020-11-18 11:42:25 +0530

    first

--- textutils/settings.py
+++ textutils/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-#django_heroku.settings(locals())
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+# STATIC_ROOT = 'staticfiles'
+
 
